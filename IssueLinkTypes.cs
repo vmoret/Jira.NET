2ce6c9505e77a6308b81f85946ad2f7384ddc4af
vmoret <moret.vincent@gmail.com>
2019-02-02 10:06:23 +0100

    Replaced IssueLinkType enum with IssueLinkTypes class This closes #19

---
+++ IssueLinkTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jira
+{
+	public static class IssueLinkTypes
+	{
+		public const string Blocker = "Blocker";
+		public const string Cloners = "Cloners";
+		public const string DependsOn = "Depends on";
+		public const string Duplicate = "Duplicate";
+		public const string Part = "Part";
+		public const string Reference = "Reference";
+	}
+}
